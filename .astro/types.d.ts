57c003a98a00c31f26327f3d8cd9ffe148f9beb7
Daniel <cayman_moods0z@icloud.com>
2024-11-23 15:06:10 +0000

    Delete .astro directory

--- .astro/types.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="astro/client" />
-/// <reference path="integrations/_astrojs_starlight/i18n-plugins.d.ts" />
-/// <reference path="integrations/starlight-sitemap-integration/types.d.ts" />
-/// <reference path="astro/content.d.ts" />
